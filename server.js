c741fe30f5a7636ec36811f0d5fe5ab3cba7fd92
mohammed hassoun <mohammed.hassoun054@gmail.com>
2026-01-19 20:45:15 +0400

    Add X-Session-ID to CORS allowed headers

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Session-ID']
 }));
 
 app.use(express.json({ limit: '50mb' }));
